972be6a71f3ef1c0e9f39b1db7de4850216c37bd
qthangus <kaitq0708@gmail.com>
2025-07-18 09:01:42 +0700

    Load and Unload Texture

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -6,27 +6,29 @@ constexpr int MONITORED_KEYS[NUM_MONITORED_KEYS] = {
     KEY_LEFT, KEY_RIGHT, KEY_UP, KEY_DOWN,
     KEY_A, KEY_S, KEY_D, KEY_W,
     KEY_SPACE, KEY_X, KEY_C, KEY_Z,
-    KEY_LEFT_CONTROL, KEY_LEFT_SHIFT, KEY_P, KEY_ESCAPE
-};
+    KEY_LEFT_CONTROL, KEY_LEFT_SHIFT, KEY_P, KEY_ESCAPE};
 
 GameManager::GameManager() : stage(nullptr) {}
 
-GameManager::~GameManager() {
+GameManager::~GameManager()
+{
     delete stage;
 }
 
-
-void GameManager::SetDifficulty(Difficulty diff) {
+void GameManager::SetDifficulty(Difficulty diff)
+{
     difficulty = diff;
 
     // Delete the old stage if it exists
-    if (stage) {
+    if (stage)
+    {
         delete stage;
         stage = nullptr;
     }
 
     // Create a new stage based on the selected difficulty
-    switch (difficulty) {
+    switch (difficulty)
+    {
     case Difficulty::Easy:
         stage = new EasyMap(); // Replace with your actual Easy stage class
         break;
@@ -42,24 +44,49 @@ void GameManager::SetDifficulty(Difficulty diff) {
     }
 }
 
-int GameManager::Update() {
+int GameManager::Update()
+{
     UpdateKeyStates();
-    if (IsKeyPressed(KEY_ESCAPE)) {
+    if (IsKeyPressed(KEY_ESCAPE))
+    {
         return menuState;
     }
-	return gameManagerState; // Continue in game manager state
+    return gameManagerState; // Continue in game manager state
 }
 
-void GameManager::UpdateKeyStates() {
+void GameManager::UpdateKeyStates()
+{
     keyStates.clear();
-    for (int i = 0; i < NUM_MONITORED_KEYS; ++i) {
+    for (int i = 0; i < NUM_MONITORED_KEYS; ++i)
+    {
         int keyCode = MONITORED_KEYS[i];
         bool isPressed = IsKeyDown(keyCode);
         keyStates.emplace_back(keyCode, isPressed);
     }
 }
 
-void GameManager::Draw() {
-	stage->Draw();  // Assuming Stage has a Draw method
+void GameManager::Draw()
+{
+    stage->Draw(); // Assuming Stage has a Draw method
     // Draw stage and game elements here
+}
+
+void GameManager::Load_Texture()
+{
+    Mario_Sprite::mario_sprite.Load_("../resources/sprite/Mario.png");
+    Luigi_Sprite::luigi_sprite.Load_("../resources/sprite/Luigi.png");
+    Item_Sprite::item_.Load_("../resources/sprite/Items_Blocks.png");
+    Font_Sprite::font_.Load_("../resources/sprite/Font.png");
+    Enemies_Sprite::enemies_.Load_("../resources/sprite/Enemies.png");
+    Screen_Sprite::screen_.Load_("../resources/sprite/GameOver_TimeUp.png");
+}
+
+void GameManager::Unload_Texture()
+{
+    Mario_Sprite::mario_sprite.Unload_();
+    Luigi_Sprite::luigi_sprite.Unload_();
+    Item_Sprite::item_.Unload_();
+    Font_Sprite::font_.Unload_();
+    Enemies_Sprite::enemies_.Unload_();
+    Screen_Sprite::screen_.Unload_();
 }
--- src/UI.cpp
+++ src/UI.cpp
@@ -4,55 +4,67 @@
 #include "ChoosingStageState.h"
 #include "Constants.h"
 #include "resource_dir.h"
-//#include "SettingScreen.h"
+// #include "SettingScreen.h"
 
-
-
-UI::UI() {
+UI::UI()
+{
     SearchAndSetResourceDir("resources");
 }
 
-void UI::Process() {
+void UI::Process()
+{
     InitWindow(Screen_w, Screen_h, "Mario Game");
     SetTargetFPS(60);
 
-    Menu* menu = new Menu();
-    ChoosingStageState* choosingStage = new ChoosingStageState();
-    GameManager* gameManager = new GameManager();
-
+    Menu *menu = new Menu();
+    ChoosingStageState *choosingStage = new ChoosingStageState();
+    GameManager *gameManager = new GameManager();
 
     int Program_state = menuState;
 
-    while (!WindowShouldClose()) {
-        if (Program_state == menuState) {
+    gameManager->Load_Texture();
+
+    while (!WindowShouldClose())
+    {
+        if (Program_state == menuState)
+        {
             Program_state = menu->Update();
         }
-        else if (Program_state == choosingStageState) {
+        else if (Program_state == choosingStageState)
+        {
             int nextState = choosingStage->Update();
-            if (nextState == gameManagerState) {
+            if (nextState == gameManagerState)
+            {
                 gameManager->SetDifficulty(choosingStage->GetSelectedDifficulty());
             }
             Program_state = nextState;
         }
-        else if (Program_state == gameManagerState) {
+        else if (Program_state == gameManagerState)
+        {
             Program_state = gameManager->Update();
         }
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
-        if (Program_state == menuState) {
+        if (Program_state == menuState)
+        {
             menu->Draw();
         }
-        else if (Program_state == choosingStageState) {
+        else if (Program_state == choosingStageState)
+        {
             choosingStage->Draw();
         }
-        else if (Program_state == gameManagerState) {
+        else if (Program_state == gameManagerState)
+        {
             gameManager->Draw();
         }
 
         EndDrawing();
     }
+
+    gameManager->Unload_Texture();
+
     delete menu;
     delete choosingStage;
     delete gameManager;
